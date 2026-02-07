c25c0c3222e6a379aaf3d4a8ec7badefcd1b3908
kajal-1221 <misskajalsingh1221@gmail.com>
2026-02-07 12:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener("click", () => {
   mobileMenu.style.display =
     mobileMenu.style.display === "flex" ? "none" : "flex";
+
 });
+
+function sendToWhatsApp() {
+  const fileInput = document.getElementById("fileInput");
+
+  if (!fileInput.files.length) {
+    alert("Please select a file first");
+    return;
+  }
+
+  const message = encodeURIComponent(
+    "Hello Masoom Photo State, I want print service. File attached.\n" +
+    "Pages: __\n" +
+    "Copies: __\n" +
+    "Color/BW: __"
+  );
+
+  const whatsappURL =
+    "https://wa.me/919525054775?text=" + message;
+
+  window.open(whatsappURL, "_blank");
+}
